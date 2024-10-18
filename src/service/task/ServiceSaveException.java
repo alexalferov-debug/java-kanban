4ca10c83a9bc580527bf743156d9b28d4d13614f
Alexey Alferov <aleksey.alferov@sibir.rt.ru>
2024-10-18 17:02:16 +0700

    Перенос строки

--- src/service/task/ServiceSaveException.java
+++ src/service/task/ServiceSaveException.java
@@ -2,8 +2,9 @@ package service.task;
 
 public class ServiceSaveException extends RuntimeException {
     public ServiceSaveException(String message, Exception e) {
-        super(message,e);
+        super(message, e);
     }
+
     public ServiceSaveException(String message) {
         super(message);
     }
